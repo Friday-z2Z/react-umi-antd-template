058712cd8f3ba38dbe012942ec52b97b8d3e4a7c
Friday-z2Z <462032310@qq.com>
2023-02-02 10:58:06 +0800

    主题修改

--- src/theme.config/default.js
+++ src/theme.config/default.js
@@ -2,9 +2,9 @@ module.exports = {
     "@font-family": "Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', SimSun, sans-serif",
     "@layout-header-height": "104px",
     "@font-size-base": "14px",
-    "@base-color": "#100E1B",
+    "@base-color": 'null',
     "@primary-color": "#1890ff",
-    "@primary-color-op": null,
-    "@match-color": null, // 与主色调搭配的第三种颜色
-    "@anticolor": "#fff"
+    "@primary-color-op": 'null',
+    "@match-color": 'null', // 与主色调搭配的第三种颜色
+    "@anticolor": 'null'
 };
