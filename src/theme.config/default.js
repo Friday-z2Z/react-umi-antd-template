3aa4c8c7201dc9030723434bb676194cfe65899e
zhangxiaowu <462032310@qq.com>
2023-02-02 10:30:36 +0800

    主题修改

--- src/theme.config/default.js
+++ src/theme.config/default.js
@@ -2,4 +2,9 @@ module.exports = {
     "@font-family": "Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', SimSun, sans-serif",
     "@layout-header-height": "104px",
     "@font-size-base": "14px",
+    "@base-color": "#100E1B",
+    "@primary-color": "#1890ff",
+    "@primary-color-op": null,
+    "@match-color": null, // 与主色调搭配的第三种颜色
+    "@anticolor": "#fff"
 };
